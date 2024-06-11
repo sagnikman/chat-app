187a21bb432aed5a1ab3fac1864c9360b3342946
Sagnik Mandal <sagnik@Sagniks-MacBook-Air.local>
2024-06-11 22:24:10 +0530

    add search functionality

--- client/src/components/sidebar/SearchInput.jsx
+++ client/src/components/sidebar/SearchInput.jsx
@@ -1,17 +1,48 @@
-import { IoIosSearch } from "react-icons/io";
+import { useState } from 'react';
+import { IoIosSearch } from 'react-icons/io';
+import useConversation from '../../store/useConversation';
+import useGetConversations from '../../hooks/useGetConversations';
+import toast from 'react-hot-toast';
 const SearchInput = () => {
+    const [search, setSearch] = useState('');
+
+    const { setSelectedConversation } = useConversation();
+
+    const { conversations } = useGetConversations();
+
+    const handleSearchSubmit = async (e) => {
+        e.preventDefault();
+        if (!search) return;
+        if (search.length < 2) {
+            return toast.error('Search text must be at least 2 characters');
+        }
+
+        const conversation = conversations.find((c) =>
+            c.fullName.toLowerCase().includes(search.toLowerCase())
+        );
+
+        if (conversation) {
+            setSelectedConversation(conversation);
+            setSearch('');
+        } else {
+            toast.error('No such user found');
+        }
+    };
+
     return (
-        <form className="flex items-center gap-2">
+        <form onSubmit={handleSearchSubmit} className="flex items-center gap-2">
             <input
                 type="text"
                 placeholder="Search"
                 className="input input-bordered rounded-full hover:bg-gray-900"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
             />
             <button
                 type="submit"
                 className="btn btn-circle bg-sky-500 text-white"
             >
-                <IoIosSearch className='w-7 h-7 outline-none'/>
+                <IoIosSearch className="w-7 h-7 outline-none" />
             </button>
         </form>
     );
