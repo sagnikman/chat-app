b7e47f24c491d6367e40caed5cfdfb1d5382d394
Sagnik Mandal <sagnik@Sagniks-MacBook-Air.local>
2024-06-12 06:42:43 +0530

    setup start and build scripts

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const { ConnectMongoDB } = require('./utils/common');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 const { app, server } = require('./socket/index');
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('');
-});
-
 app.use('/api', apiRoutes);
 
 PORT = ServerConfig.PORT || 3000;
+
+app.use(express.static(path.join(path.resolve(), 'client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'client', 'dist', 'index.html'));
+});
 
 server.listen(PORT, () => {
     ConnectMongoDB.connectToMongoDB();
